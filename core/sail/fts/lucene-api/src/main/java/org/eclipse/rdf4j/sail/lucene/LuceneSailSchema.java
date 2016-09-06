80c4ac534a88324cff3317bd598937f5cac1f7d8
Jacek Grzebyta <grzebyta.dev@gmail.com>
2016-09-06 17:36:08 +0100

    Clean code
    Remove LuceneSail2Schema
    Remove LuceneSearch
    Remove non existing service from list
    
    Signed-off-by:Jacek Grzebyta <grzebyta.dev@gmail.com>

--- core/sail/fts/lucene-api/src/main/java/org/eclipse/rdf4j/sail/lucene/LuceneSailSchema.java
+++ core/sail/fts/lucene-api/src/main/java/org/eclipse/rdf4j/sail/lucene/LuceneSailSchema.java
@@ -13,6 +13,33 @@ import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 
 /**
  * LuceneSailSchema defines predicates that can be used for expressing a Lucene query in a RDF query.
+ * <h2>Syntax for property function</h2>
+ * <p>
+ * <code>search:search(query, subject, propertyPredicate, property, scorePredicate, snippetPredicate) [functional,
+ * not spin syntax]</code><br/>
+ * where:
+ * <ol>
+ * <li><i>query</i> is the query string
+ * <li><i>subject</i> is the query subject or the constant <i>search:allMatches</i>
+ * <li><i>propertyPredicate</i> is the constant <i>search:property</i> or not present
+ * <li><i>property</i> is present if only propertyPredicate is present and is the property to query or the
+ * constant <i>search:allProperties<i>
+ * <li><i>scorePredicate</i> is the constant <i>search:score</i> or not present
+ * <li><i>snippetPredicate</i> is the constant <i>search:snippet</i> or not present
+ * </ol>
+ * </p>
+ * <br/>
+ * <p>
+ * The output should be in this order: <code>subject, property, score, snippet</code><br/>
+ * where:
+ * <ol>
+ * <li><i>subject</i> is included if the subject parameter is <i>search:allMatches</i> else omitted
+ * <li><i>property</i> included if the propertyPredicate parameter is present and the property parameter is
+ * <i>search:allProperties</i> else omitted
+ * <li><i>score</i> is included if the scorePredicate is <i>search:score</i> else omitted
+ * <li><i>snippet</i> is included if the snippetPredicate is <i>search:snippet</i> else omitted
+ * </ol>
+ * </p>
  */
 public class LuceneSailSchema {
 
